45829f79cfc097c4684b5648e8b35f0f0dae8f7a
Vander Lima <vnd.vander@gmail.com>
2023-09-06 14:00:54 -0300

    feat: structure initial Home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,29 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          alt='Calendário simbolizando aplicação em funcionamento'
+          height={400}
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
+
