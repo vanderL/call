7e6f658cd003d1d8264eeb4afb09a38a288449cb
Vander Lima <vnd.vander@gmail.com>
2023-09-05 22:09:17 -0300

    chore: lint config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { getCssText } from "@ignite-ui/react";
+import { getCssText } from '@ignite-ui/react'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
@@ -6,12 +6,20 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
 
-
-
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
