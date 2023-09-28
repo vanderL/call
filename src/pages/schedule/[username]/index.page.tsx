cd06b1368b72257a46e5a3c779bd2f16ba8e1c41
Vander Lima <vnd.vander@gmail.com>
2023-09-28 12:46:01 -0300

    feat: getStatic for profile user |  page schedule

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,58 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { Container, UserHeader } from './styles'
+import { prisma } from '@/src/lib/prisma'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+    </Container>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
